027fd635a7716413ddc4d663ebe2726f76b70d28
DESKTOP\crisc <criscamilo2070@gmail.com>
2024-11-27 21:45:26 -0500

    test components

---
+++ allio-front/src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      index navbar 123
+      <button className='btn btn-primary'>boton</button>
+    </div>
+  )
+}
+
+export default Navbar
