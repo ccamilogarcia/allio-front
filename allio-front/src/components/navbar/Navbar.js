81c9ef4aaa92f5bef94b66c2b6acb1407ad8123b
DESKTOP\crisc <criscamilo2070@gmail.com>
2024-11-27 21:55:11 -0500

    test git & github

--- allio-front/src/components/navbar/Navbar.js
+++ allio-front/src/components/navbar/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      index navbar 123
+      index navbar 123 - test git & github
       <button className='btn btn-primary'>boton</button>
     </div>
   )
